e0af111a21898df0f91b571f6cbb31806ccdf56c
cylian <dopaminel@foxmail.com>
2023-02-11 00:55:35 +0800

    fix: details of run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 OMP_NUM_THREADS=1 python -m torch.distributed.launc
     --epochs 200 \
     --warmup_epochs 40 \
     --blr 1.5e-4 --weight_decay 0.05 \
-    --resume mae_pretrain_vit_base.pth \
+    --resume /path/to/pretrained/weights \
     --data_path /path/to/dataset/ \
     --output_dir /path/to/output/ \
